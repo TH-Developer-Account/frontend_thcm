d0134b0decb96682cade8fbc9c42253ff39bcb45
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-03-05 16:30:48 +0530

    assign user changes

--- src/modules/admin/user-profile/components/ProfileList.tsx
+++ src/modules/admin/user-profile/components/ProfileList.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { PlusIcon } from "lucide-react";
+import { useAuth } from "../../../../context/Auth/useAuth";
+import { ServerAxios } from "../../../../services/ServerAxios";
 import Button from "../../../../components/common/Button";
 import { Modal } from "../../../../components/common/Modal";
 import { Alert } from "../../../../components/common/Alert";
@@ -7,178 +9,205 @@ import type { Profile } from "../types/profile.types";
 import Avatar from "../../../../components/common/Avatar";
 import { SearchInput } from "../../../../components/FormElements/SearchInput";
 import { AssignUsers } from "./AssignUsers";
+import { useToast } from "../../../../context/Auth/AuthContext";
 
 type ProfileListProps = {
-	profiles: Profile[];
-	onCreateNew: () => void;
-	onEdit: (profile: Profile) => void;
-	onDelete: (id: string) => void;
-	onEditModal?: (id: string) => void;
-	activeTab?: string;
-	counts?: Record<string, number>;
-	onTabChange?: (tab: string) => void;
-	search: string;
-	onSearchChange: (value: string) => void;
+  profiles: Profile[];
+  onCreateNew: () => void;
+  onEdit: (profile: Profile) => void;
+  onDelete: (id: string) => void;
+  onEditModal?: (id: string) => void;
+  activeTab?: string;
+  counts?: Record<string, number>;
+  onTabChange?: (tab: string) => void;
+  search: string;
+  onSearchChange: (value: string) => void;
 };
 
 const ProfileList: React.FC<ProfileListProps> = ({
-	profiles,
-	onCreateNew,
-	search,
-	onSearchChange,
-	onEdit,
-	onDelete,
+  profiles,
+  onCreateNew,
+  search,
+  onSearchChange,
+  onEdit,
+  onDelete,
 }) => {
-	const [deleteModal, setDeleteModal] = useState<Profile | null>(null);
-	const [assignModalOpen, setAssignModalOpen] = useState(false);
+  const { workspaceId } = useAuth();
+  const { showToast } = useToast();
+  const [deleteModal, setDeleteModal] = useState<Profile | null>(null);
+  const [assignModalOpen, setAssignModalOpen] = useState<string | null>(null);
 
-	return (
-		<>
-			<div className="max-w-full mx-auto py-3 h-full  min-h-screen">
-				<div className="flex justify-between my-6 px-3">
-					<h2 className="text-2xl">User Profiles</h2>
-					<div className="flex gap-2 items-center">
-						<div className="">
-							<div className="search">
-								<SearchInput
-									placeholder="Search ..."
-									value={search}
-									onChange={onSearchChange}
-								/>
-							</div>
-						</div>
-						<Button
-							status="Brand"
-							size="lg"
-							text="Assign Users"
-							onClick={() => setAssignModalOpen(true)}
-							Icon={PlusIcon}
-						/>
-						<Button
-							status="Brand"
-							size="lg"
-							text="New Profile"
-							onClick={onCreateNew}
-							Icon={PlusIcon}
-						/>
-					</div>
-				</div>
-				<div className="bg-white rounded-t-2xl mt-2 border border-gray-200 border-b-0 h-full">
-					<div className="grid grid-cols-1  gap-4 mt-4">
-						<div className="bg-white overflow-y-auto ">
-							<table className="w-full text-sm">
-								<thead className="bg-gray-100 text-gray-600">
-									<tr>
-										<th className="px-6 py-4 text-left">Profile Name</th>
-										<th className="px-6 py-4 text-left">Profile Description</th>
-										<th className="px-6 py-4 text-left">User Count</th>
-										<th className="px-6 py-4 text-left" colSpan={2}>
-											Users
-										</th>
-										<th className="px-6 py-4 text-center">Actions</th>
-									</tr>
-								</thead>
+  const handleAssignUser = async (
+    userIds: string[],
+    profileId: string | null,
+  ): Promise<void> => {
+    try {
+      const {
+        data: { message },
+      } = await ServerAxios.post("/users/assign-profile", {
+        userIds,
+        profileId,
+        workspaceId,
+      });
+      showToast({
+        type: "success",
+        title: "Success",
+        description: message,
+      });
+    } catch (error) {
+      console.error("Failed to assign users:", error);
+    } finally {
+      setAssignModalOpen(null);
+    }
+  };
 
-								<tbody>
-									{profiles.map((profile, idx) => (
-										<tr
-											key={profile.id || idx}
-											className="border-t border-b border-gray-200 hover:bg-gray-50 transition text-left "
-										>
-											<td className="px-6 py-4  items-center gap-3">
-												<h3 className="">{profile.name}</h3>
-											</td>
+  return (
+    <>
+      <div className="max-w-full mx-auto py-3 h-full  min-h-screen">
+        <div className="flex justify-between my-6 px-3">
+          <h2 className="text-2xl">User Profiles</h2>
+          <div className="flex gap-2 items-center">
+            <div className="">
+              <div className="search">
+                <SearchInput
+                  placeholder="Search ..."
+                  value={search}
+                  onChange={onSearchChange}
+                />
+              </div>
+            </div>
+            <Button
+              status="Brand"
+              size="lg"
+              text="New Profile"
+              onClick={onCreateNew}
+              Icon={PlusIcon}
+            />
+          </div>
+        </div>
+        <div className="bg-white rounded-t-2xl mt-2 border border-gray-200 border-b-0 h-full">
+          <div className="grid grid-cols-1  gap-4 mt-4">
+            <div className="bg-white overflow-y-auto ">
+              <table className="w-full text-sm">
+                <thead className="bg-gray-100 text-gray-600">
+                  <tr>
+                    <th className="px-6 py-4 text-left">Profile Name</th>
+                    <th className="px-6 py-4 text-left">Profile Description</th>
+                    <th className="px-6 py-4 text-left">User Count</th>
+                    <th className="px-6 py-4 text-left" colSpan={2}>
+                      Users
+                    </th>
+                    <th className="px-6 py-4 text-center">Actions</th>
+                  </tr>
+                </thead>
 
-											<td className="px-6 py-4 items-center">
-												<p>{profile.description}</p>
-											</td>
-											<td className="px-6 py-4 items-center text-center">
-												<p>{profile.assignedUserCount}</p>
-											</td>
-											<td
-												className="px-6 py-4 col-span-2 items-center"
-												colSpan={2}
-											>
-												<div className="flex items-center">
-													{profile.users && profile.users.length > 0 ? (
-														<>
-															<div className="flex -space-x-2">
-																{profile.users.slice(0, 3).map((user) => (
-																	<div key={user.id}>
-																		<Avatar
-																			size="sm"
-																			firstName={user.firstName}
-																			lastName={user.lastName}
-																			className="border-2 border-white rounded-full shadow-sm"
-																		/>
-																	</div>
-																))}
-															</div>
+                <tbody>
+                  {profiles.map((profile, idx) => (
+                    <tr
+                      key={profile.id || idx}
+                      className="border-t border-b border-gray-200 hover:bg-gray-50 transition text-left "
+                    >
+                      <td className="px-6 py-4  items-center gap-3">
+                        <h3 className="">{profile.name}</h3>
+                      </td>
 
-															{profile.users.length > 3 && (
-																<div className="ml-1 text-xs font-medium text-gray-500">
-																	+{profile.users.length - 3}
-																</div>
-															)}
-														</>
-													) : (
-														<div className="text-gray-400 text-sm">
-															No Users Assigned.
-														</div>
-													)}
-												</div>
-											</td>
+                      <td className="px-6 py-4 items-center">
+                        <p>{profile.description}</p>
+                      </td>
+                      <td className="px-6 py-4 items-center text-center">
+                        <p>{profile.assignedUserCount}</p>
+                      </td>
+                      <td
+                        className="px-6 py-4 col-span-2 items-center"
+                        colSpan={2}
+                      >
+                        <div className="flex items-center">
+                          {profile.users && profile.users.length > 0 ? (
+                            <>
+                              <div className="flex -space-x-2">
+                                {profile.users.slice(0, 3).map((user) => (
+                                  <div key={user.id}>
+                                    <Avatar
+                                      size="sm"
+                                      firstName={user.firstName}
+                                      lastName={user.lastName}
+                                      className="border-2 border-white rounded-full shadow-sm"
+                                    />
+                                  </div>
+                                ))}
+                              </div>
 
-											<td className="px-6 py-4 text-right items-center">
-												<div className="flex gap-2	 justify-end">
-													<Button
-														size="sm"
-														text="Edit"
-														variant="primary"
-														onClick={() => onEdit(profile)}
-													/>
-													<Button
-														size="sm"
-														variant="danger"
-														text="Delete"
-														onClick={() => setDeleteModal(profile)}
-													/>
-												</div>
-											</td>
-										</tr>
-									))}
-								</tbody>
-							</table>
-						</div>
-					</div>
-				</div>
-			</div>
-			<Modal open={!!deleteModal} onClose={() => setDeleteModal(null)}>
-				<Alert
-					variant="warning"
-					title="Delete Profile"
-					description={`Are you sure you want to delete "${deleteModal?.name}"?`}
-					primaryAction={{
-						label: "Delete",
-						onClick: () => {
-							if (deleteModal) {
-								onDelete(deleteModal.id);
-								setDeleteModal(null);
-							}
-						},
-					}}
-					secondaryAction={{
-						label: "Cancel",
-						onClick: () => setDeleteModal(null),
-					}}
-				/>
-			</Modal>
-			<AssignUsers
-				open={assignModalOpen}
-				onClose={() => setAssignModalOpen(false)}
-			/>
-		</>
-	);
+                              {profile.users.length > 3 && (
+                                <div className="ml-1 text-xs font-medium text-gray-500">
+                                  +{profile.users.length - 3}
+                                </div>
+                              )}
+                            </>
+                          ) : (
+                            <div className="text-gray-400 text-sm">
+                              No Users Assigned.
+                            </div>
+                          )}
+                        </div>
+                      </td>
+
+                      <td className="px-6 py-4 text-right items-center">
+                        <div className="flex gap-2	 justify-end">
+                          <Button
+                            size="sm"
+                            text="Assign User"
+                            variant="primary"
+                            onClick={() => setAssignModalOpen(profile.id)}
+                          />
+                          <Button
+                            size="sm"
+                            text="Edit"
+                            variant="primary"
+                            onClick={() => onEdit(profile)}
+                          />
+                          <Button
+                            size="sm"
+                            variant="danger"
+                            text="Delete"
+                            onClick={() => setDeleteModal(profile)}
+                          />
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Modal open={!!deleteModal} onClose={() => setDeleteModal(null)}>
+        <Alert
+          variant="warning"
+          title="Delete Profile"
+          description={`Are you sure you want to delete "${deleteModal?.name}"?`}
+          primaryAction={{
+            label: "Delete",
+            onClick: () => {
+              if (deleteModal) {
+                onDelete(deleteModal.id);
+                setDeleteModal(null);
+              }
+            },
+          }}
+          secondaryAction={{
+            label: "Cancel",
+            onClick: () => setDeleteModal(null),
+          }}
+        />
+      </Modal>
+      <AssignUsers
+        profileId={assignModalOpen}
+        onClose={() => setAssignModalOpen(null)}
+        handleAssignUser={handleAssignUser}
+      />
+    </>
+  );
 };
 
 export default ProfileList;
--- src/modules/admin/user-profile/types/profile.types.ts
+++ src/modules/admin/user-profile/types/profile.types.ts
@@ -1,96 +1,96 @@
 export type RoleType =
-	| "admin"
-	| "manager"
-	| "analyst"
-	| "executive"
-	| "agent"
-	| "viewer";
+  | "admin"
+  | "manager"
+  | "analyst"
+  | "executive"
+  | "agent"
+  | "viewer";
 
 export type PermissionFlag = {
-	read: boolean;
-	write: boolean;
+  read: boolean;
+  write: boolean;
 };
 
 export type PermissionMap = Record<string, PermissionFlag>;
 
 export type User = {
-	id: string;
-	firstName: string;
-	lastName: string;
-	email: string;
-	jobRole: string;
-	phone: string;
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  //   jobRole: string;
+  phone: string;
 };
-export type C4CEmployeeResponse = {
-	CEE_UUID: string;
-	CEE_GIVEN_NAME: string;
-	CEE_FAMILY_NAME: string;
-	CWRKADRS_EMAIL: string;
-	TJOB_UUID: string;
-	CWRKADRS_FRM_MOBILE: string;
+export type UserResponse = {
+  id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  //   TJOB_UUID: string;
+  phone_number: string;
 };
-export const mapC4CEmployeeToUser = (emp: C4CEmployeeResponse): User => ({
-	id: emp.CEE_UUID,
-	firstName: emp.CEE_GIVEN_NAME,
-	lastName: emp.CEE_FAMILY_NAME,
-	email: emp.CWRKADRS_EMAIL,
-	jobRole: emp.TJOB_UUID,
-	phone: emp.CWRKADRS_FRM_MOBILE,
+export const mapUser = (emp: UserResponse): User => ({
+  id: emp.id,
+  firstName: emp.first_name,
+  lastName: emp.last_name,
+  email: emp.email,
+  //   jobRole: emp.TJOB_UUID,
+  phone: emp.phone_number,
 });
 
 export interface Profile {
-	id: string;
-	assignedUserCount: number;
-	isSystemProfile: boolean;
-	name: string;
-	description: string;
-	users: User[];
-	permissions: ApiPermission[];
+  id: string;
+  assignedUserCount: number;
+  isSystemProfile: boolean;
+  name: string;
+  description: string;
+  users: User[];
+  permissions: ApiPermission[];
 }
 
 export interface WorkspaceAccess {
-	id: string;
-	name: string;
-	apps: AppAccess[];
+  id: string;
+  name: string;
+  apps: AppAccess[];
 }
 
 export interface AppAccess {
-	key: string;
-	name: string;
-	enabled: boolean;
-	modules: ModuleAccess[];
+  key: string;
+  name: string;
+  enabled: boolean;
+  modules: ModuleAccess[];
 }
 
 export interface ModuleAccess {
-	key: string;
-	name: string;
-	profiles: ProfileAccess[];
+  key: string;
+  name: string;
+  profiles: ProfileAccess[];
 }
 
 export interface ProfileAccess {
-	id: string;
-	permissions: Permission[];
+  id: string;
+  permissions: Permission[];
 }
 
 export type Permission = "read" | "write";
 
 export type WorkspacePermission = {
-	appKey: string;
-	moduleKey: string;
-	action: Permission;
+  appKey: string;
+  moduleKey: string;
+  action: Permission;
 };
 
 export type WorkspacePayload = WorkspacePermission[];
 
 export interface ApiPermission {
-	appKey: string;
-	moduleKey: string;
-	action: Permission;
+  appKey: string;
+  moduleKey: string;
+  action: Permission;
 }
 
 export type PermissionFlags = {
-	read: boolean;
-	write: boolean;
+  read: boolean;
+  write: boolean;
 };
 
 export type PermState = Record<string, Record<string, PermissionFlags>>;
@@ -98,12 +98,12 @@ export type PermState = Record<string, Record<string, PermissionFlags>>;
 export type Action = "read" | "write";
 
 export interface Module {
-	key: string;
-	name: string;
+  key: string;
+  name: string;
 }
 
 export interface AppItem {
-	key: string;
-	name: string;
-	modules: Module[];
+  key: string;
+  name: string;
+  modules: Module[];
 }
