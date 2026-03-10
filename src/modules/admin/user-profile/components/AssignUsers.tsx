4a0e32a4709859c88c755cf6efa560a845e44709
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-03-10 09:49:04 +0530

    coming soon page, profile page theme fix

--- src/modules/admin/user-profile/components/AssignUsers.tsx
+++ src/modules/admin/user-profile/components/AssignUsers.tsx
@@ -8,144 +8,144 @@ import { Badge } from "../../../../components/common/Badge";
 import { SearchInput } from "../../../../components/FormElements/SearchInput";
 
 type AssignProps = {
-  profileId: string | null;
-  onClose: () => void;
-  handleAssignUser: (
-    userIds: string[],
-    profileId: string | null,
-  ) => Promise<void>;
+	profileId: string | null;
+	onClose: () => void;
+	handleAssignUser: (
+		userIds: string[],
+		profileId: string | null,
+	) => Promise<void>;
 };
 
 export const AssignUsers: React.FC<AssignProps> = ({
-  profileId,
-  onClose,
-  handleAssignUser,
+	profileId,
+	onClose,
+	handleAssignUser,
 }) => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState<string>("");
+	const [users, setUsers] = useState<User[]>([]);
+	const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
+	const [loading, setLoading] = useState(false);
+	const [search, setSearch] = useState<string>("");
 
-  useEffect(() => {
-    if (!profileId) return;
+	useEffect(() => {
+		if (!profileId) return;
 
-    const loadUsers = async () => {
-      try {
-        setLoading(true);
-        const { data } = await ServerAxios.get("/users");
-        console.log({ data });
-        const mappedUsers: User[] = data.map(mapUser);
-        setUsers(mappedUsers);
-      } catch (err) {
-        console.error("Failed to fetch users", err);
-      } finally {
-        setLoading(false);
-      }
-    };
+		const loadUsers = async () => {
+			try {
+				setLoading(true);
+				const { data } = await ServerAxios.get("/users");
+				console.log({ data });
+				const mappedUsers: User[] = data.map(mapUser);
+				setUsers(mappedUsers);
+			} catch (err) {
+				console.error("Failed to fetch users", err);
+			} finally {
+				setLoading(false);
+			}
+		};
 
-    loadUsers();
-  }, [profileId]);
+		loadUsers();
+	}, [profileId]);
 
-  // Filter users
-  const filteredUsers = useMemo(() => {
-    return users.filter((user) =>
-      user.firstName?.toLowerCase().includes(search.toLowerCase()),
-    );
-  }, [users, search]);
+	// Filter users
+	const filteredUsers = useMemo(() => {
+		return users.filter((user) =>
+			user.firstName?.toLowerCase().includes(search.toLowerCase()),
+		);
+	}, [users, search]);
 
-  const toggleUser = (id: string) => {
-    setSelectedUsers((prev) =>
-      prev.includes(id) ? prev.filter((u) => u !== id) : [...prev, id],
-    );
-  };
+	const toggleUser = (id: string) => {
+		setSelectedUsers((prev) =>
+			prev.includes(id) ? prev.filter((u) => u !== id) : [...prev, id],
+		);
+	};
 
-  return (
-    <Modal open={!!profileId} onClose={onClose}>
-      <div className="w-[1000px] max-h-[520px] flex flex-col mx-auto bg-white p-4 rounded-xl">
-        {/* Header */}
-        <div className="flex items-center justify-between mb-6">
-          <h2 className="text-base font-bold text-zinc-100 flex items-center gap-2">
-            <span>👥</span> Assign Users
-          </h2>
-          <Badge variant="primary">Users</Badge>
-        </div>
-        {/* Search */}
-        <div className="relative mb-4">
-          <SearchInput value={search} onChange={setSearch} />
-        </div>
+	return (
+		<Modal open={!!profileId} onClose={onClose}>
+			<div className="w-[1000px] max-h-[520px] flex flex-col mx-auto bg-white p-4 rounded-xl">
+				{/* Header */}
+				<div className="flex items-center justify-between mb-6">
+					<h2 className="text-base font-bold text-zinc-900 flex items-center gap-2">
+						<span>👥</span> Assign Users
+					</h2>
+					<Badge variant="primary">Users</Badge>
+				</div>
+				{/* Search */}
+				<div className="relative mb-4">
+					<SearchInput value={search} onChange={setSearch} />
+				</div>
 
-        {/* Content */}
-        <div className="space-y-2 overflow-y-auto p-6 flex-1">
-          {loading ? (
-            <p className="text-sm text-gray-400">Loading users...</p>
-          ) : (
-            filteredUsers.map((user) => {
-              const selected = selectedUsers.includes(user.id);
+				{/* Content */}
+				<div className="space-y-2 overflow-y-auto p-6 flex-1">
+					{loading ? (
+						<p className="text-sm text-gray-400">Loading users...</p>
+					) : (
+						filteredUsers.map((user) => {
+							const selected = selectedUsers.includes(user.id);
 
-              return (
-                <div
-                  key={user.id}
-                  onClick={() => toggleUser(user.id)}
-                  className={`flex items-center gap-3 p-3 rounded-xl border cursor-pointer transition-all duration-150 ${
-                    selected
-                      ? "bg-amber-500/5 border-amber-500/30"
-                      : "bg-gray-100/40 border-gray-200 hover:border-gray-300 hover:bg-gray-100"
-                  }`}
-                >
-                  <div>
-                    <Avatar
-                      firstName={user.firstName}
-                      lastName={user.lastName}
-                    />
-                  </div>
+							return (
+								<div
+									key={user.id}
+									onClick={() => toggleUser(user.id)}
+									className={`flex items-center gap-3 p-3 rounded-xl border cursor-pointer transition-all duration-150 ${
+										selected
+											? "bg-amber-500/5 border-amber-500/30"
+											: "bg-gray-100/40 border-gray-200 hover:border-gray-300 hover:bg-gray-100"
+									}`}
+								>
+									<div>
+										<Avatar
+											firstName={user.firstName}
+											lastName={user.lastName}
+										/>
+									</div>
 
-                  <div className=" min-w-0">
-                    <p className="text-sm font-semibold truncate">
-                      {user.firstName} {user.lastName}
-                    </p>
-                  </div>
-                  <div className="flex-1 min-w-0">
-                    <p className="text-xs text-gray-500 truncate ">
-                      {user.email ?? "example@tatahitachi.co.in"}
-                    </p>
-                  </div>
-                  {/* <div className="flex-1 min-w-0">
+									<div className=" min-w-0">
+										<p className="text-sm font-semibold truncate">
+											{user.firstName} {user.lastName}
+										</p>
+									</div>
+									<div className="flex-1 min-w-0">
+										<p className="text-xs text-gray-500 truncate ">
+											{user.email ?? "example@tatahitachi.co.in"}
+										</p>
+									</div>
+									{/* <div className="flex-1 min-w-0">
                     <p className="text-xs text-gray-500 truncate">
                       {user.jobRole ?? "User"}
                     </p>
                   </div> */}
-                  <div className="flex-1 min-w-0">
-                    <p className="text-xs text-gray-500 truncate">
-                      {user.phone ?? "914******7"}
-                    </p>
-                  </div>
-                  <div
-                    className={`w-5 h-5 rounded-md border flex items-center justify-center ${
-                      selected
-                        ? "bg-amber-500 border-amber-500"
-                        : "border-gray-300"
-                    }`}
-                  >
-                    {selected && (
-                      <span className="text-white text-xs font-bold">✓</span>
-                    )}
-                  </div>
-                </div>
-              );
-            })
-          )}
-        </div>
+									<div className="flex-1 min-w-0">
+										<p className="text-xs text-gray-500 truncate">
+											{user.phone ?? "914******7"}
+										</p>
+									</div>
+									<div
+										className={`w-5 h-5 rounded-md border flex items-center justify-center ${
+											selected
+												? "bg-amber-500 border-amber-500"
+												: "border-gray-300"
+										}`}
+									>
+										{selected && (
+											<span className="text-white text-xs font-bold">✓</span>
+										)}
+									</div>
+								</div>
+							);
+						})
+					)}
+				</div>
 
-        {/* Footer */}
-        <div className="p-4 border-t flex justify-end gap-3">
-          <Button text="Cancel" variant="primary" onClick={onClose} />
-          <Button
-            text="Assign Users"
-            status="Brand"
-            onClick={() => handleAssignUser(selectedUsers, profileId)}
-          />
-        </div>
-      </div>
-    </Modal>
-  );
+				{/* Footer */}
+				<div className="p-4 border-t flex justify-end gap-3">
+					<Button text="Cancel" variant="primary" onClick={onClose} />
+					<Button
+						text="Assign Users"
+						status="Brand"
+						onClick={() => handleAssignUser(selectedUsers, profileId)}
+					/>
+				</div>
+			</div>
+		</Modal>
+	);
 };
--- src/modules/admin/user-profile/components/ProfileFormPage.tsx
+++ src/modules/admin/user-profile/components/ProfileFormPage.tsx
@@ -18,168 +18,168 @@ import { usePermissionMatrix } from "../hooks/userPermissionMatrix";
 import type { Profile, WorkspacePayload } from "../types/profile.types";
 
 export const ProfileFormPage = () => {
-  const { id } = useParams();
-  const isEditing = Boolean(id);
-  const [activeSection, setActiveSection] = useState<"general" | "permissions">(
-    "general",
-  );
-  const [isLoading, setIsLoading] = useState(false);
-  const [search, setSearch] = useState("");
-  const [collapsed, setCollapsed] = useState<Record<string, boolean>>({});
-  const [form, setForm] = useState<Profile>({
-    id: "",
-    name: "",
-    description: "",
-    assignedUserCount: 0,
-    isSystemProfile: false,
-    users: [],
-    permissions: [],
-  });
-  const navigate = useNavigate();
-  const { workspaceId } = useAuth();
-  const { showToast } = useToast();
-  const { theme } = useTheme();
+	const { id } = useParams();
+	const isEditing = Boolean(id);
+	const [activeSection, setActiveSection] = useState<"general" | "permissions">(
+		"general",
+	);
+	const [isLoading, setIsLoading] = useState(false);
+	const [search, setSearch] = useState("");
+	const [collapsed, setCollapsed] = useState<Record<string, boolean>>({});
+	const [form, setForm] = useState<Profile>({
+		id: "",
+		name: "",
+		description: "",
+		assignedUserCount: 0,
+		isSystemProfile: false,
+		users: [],
+		permissions: [],
+	});
+	const navigate = useNavigate();
+	const { workspaceId } = useAuth();
+	const { showToast } = useToast();
+	const { theme } = useTheme();
 
-  // 🔥 All permission logic comes from hook
-  const {
-    permState,
-    togglePerm,
-    toggleAppAll,
-    toggleAppAction,
-    appActionState,
-  } = usePermissionMatrix(form.permissions);
+	// 🔥 All permission logic comes from hook
+	const {
+		permState,
+		togglePerm,
+		toggleAppAll,
+		toggleAppAction,
+		appActionState,
+	} = usePermissionMatrix(form.permissions);
 
-  useEffect(() => {
-    const loadProfileToBeUpdated = async () => {
-      try {
-        const { data } = await ServerAxios.get(`/profile/${id}`);
+	useEffect(() => {
+		const loadProfileToBeUpdated = async () => {
+			try {
+				const { data } = await ServerAxios.get(`/profile/${id}`);
 
-        console.log({ data });
-        setForm(data);
-      } catch (error: unknown) {
-        if (error instanceof Error) {
-          console.error("Failed to load profiles:", error?.message);
-        } else {
-          console.error("Failed to load profiles:", error);
-        }
-      }
-    };
+				console.log({ data });
+				setForm(data);
+			} catch (error: unknown) {
+				if (error instanceof Error) {
+					console.error("Failed to load profiles:", error?.message);
+				} else {
+					console.error("Failed to load profiles:", error);
+				}
+			}
+		};
 
-    if (id) {
-      loadProfileToBeUpdated();
-    }
-  }, [id]);
+		if (id) {
+			loadProfileToBeUpdated();
+		}
+	}, [id]);
 
-  const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-  ) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  };
+	const handleChange = (
+		e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+	) => {
+		const { name, value } = e.target;
+		setForm((prev) => ({ ...prev, [name]: value }));
+	};
 
-  const handleSave = async (permissions: WorkspacePayload) => {
-    try {
-      setIsLoading(true);
-      const payload = {
-        name: form?.name, // optional
-        description: form?.description, // optional
-        permissions: permissions, // required
-      };
+	const handleSave = async (permissions: WorkspacePayload) => {
+		try {
+			setIsLoading(true);
+			const payload = {
+				name: form?.name, // optional
+				description: form?.description, // optional
+				permissions: permissions, // required
+			};
 
-      console.log("Submitting Payload =>", payload);
-      let apiResponseMessage;
-      if (isEditing) {
-        const {
-          data: { message },
-        } = await ServerAxios.patch(`/profile/update/${id}`, payload);
-        apiResponseMessage = message;
-      } else {
-        const {
-          data: { message },
-        } = await ServerAxios.post(`/profile/create`, {
-          workspaceId,
-          ...payload,
-        });
-        apiResponseMessage = message;
-      }
+			console.log("Submitting Payload =>", payload);
+			let apiResponseMessage;
+			if (isEditing) {
+				const {
+					data: { message },
+				} = await ServerAxios.patch(`/profile/update/${id}`, payload);
+				apiResponseMessage = message;
+			} else {
+				const {
+					data: { message },
+				} = await ServerAxios.post(`/profile/create`, {
+					workspaceId,
+					...payload,
+				});
+				apiResponseMessage = message;
+			}
 
-      showToast({
-        type: "success",
-        title: apiResponseMessage,
-      });
+			showToast({
+				type: "success",
+				title: apiResponseMessage,
+			});
 
-      navigate("/admin/user_profiles");
-    } catch (error) {
-      console.error("Failed to update profile:", error);
+			navigate("/admin/user_profiles");
+		} catch (error) {
+			console.error("Failed to update profile:", error);
 
-      showToast({
-        type: "error",
-        title: "Failed to update profile",
-      });
-    } finally {
-      setIsLoading(false);
-    }
-  };
+			showToast({
+				type: "error",
+				title: "Failed to update profile",
+			});
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  const filteredApps = apps.map((app) => ({
-    ...app,
-    modules: app.modules.filter((m) =>
-      m.name.toLowerCase().includes(search.toLowerCase()),
-    ),
-  }));
+	const filteredApps = apps.map((app) => ({
+		...app,
+		modules: app.modules.filter((m) =>
+			m.name.toLowerCase().includes(search.toLowerCase()),
+		),
+	}));
 
-  console.log({ permState });
+	console.log({ permState });
 
-  return (
-    <div
-      className="bg-white 0.3s ease max-w-full mx-auto h-full min-h-screen"
-      data-theme={theme}
-    >
-      <div className="max-w-full mx-auto h-full p-4">
-        {/* Header */}
-        <div className="flex items-center gap-4 mb-8">
-          <button
-            onClick={() => navigate("/admin/user_profiles")}
-            className="w-9 h-9 rounded-lg bg-zinc-900 border border-zinc-800 flex items-center justify-center"
-          >
-            <ArrowLeft />
-          </button>
+	return (
+		<div
+			className="bg-white 0.3s ease max-w-full mx-auto h-full min-h-screen"
+			data-theme={theme}
+		>
+			<div className="max-w-full mx-auto h-full p-4">
+				{/* Header */}
+				<div className="flex items-center gap-4 mb-8">
+					<button
+						onClick={() => navigate("/admin/user_profiles")}
+						className="w-9 h-9 rounded-lg bg-gray-50 border border-zinc-800 flex items-center justify-center"
+					>
+						<ArrowLeft />
+					</button>
 
-          <h2 className="text-xl font-bold text-zinc-50">
-            {isEditing ? `Editing: ${form?.name}` : "New User Profile"}
-          </h2>
-        </div>
+					<h2 className="text-xl font-bold text-zinc-900">
+						{isEditing ? `Editing: ${form?.name}` : "New User Profile"}
+					</h2>
+				</div>
 
-        {/* Sections */}
-        {activeSection === "general" && (
-          <ProfileGeneralSection
-            form={form}
-            isEditing={isEditing}
-            handleChange={handleChange}
-            setForm={setForm}
-            onCancel={() => navigate("/admin/user_profiles")}
-            onSubmit={() => {}}
-            onPermission={() => setActiveSection("permissions")}
-          />
-        )}
+				{/* Sections */}
+				{activeSection === "general" && (
+					<ProfileGeneralSection
+						form={form}
+						isEditing={isEditing}
+						handleChange={handleChange}
+						setForm={setForm}
+						onCancel={() => navigate("/admin/user_profiles")}
+						onSubmit={() => {}}
+						onPermission={() => setActiveSection("permissions")}
+					/>
+				)}
 
-        {activeSection === "permissions" && (
-          <PermissionMatrix
-            filteredApps={filteredApps}
-            collapsed={collapsed}
-            permState={permState}
-            search={search}
-            setSearch={setSearch}
-            setCollapsed={setCollapsed}
-            appActionState={appActionState}
-            toggleAppAll={toggleAppAll}
-            toggleAppAction={toggleAppAction}
-            togglePerm={togglePerm}
-            onSavePermissions={handleSave}
-            isLoading={isLoading}
-          />
-        )}
-      </div>
-    </div>
-  );
+				{activeSection === "permissions" && (
+					<PermissionMatrix
+						filteredApps={filteredApps}
+						collapsed={collapsed}
+						permState={permState}
+						search={search}
+						setSearch={setSearch}
+						setCollapsed={setCollapsed}
+						appActionState={appActionState}
+						toggleAppAll={toggleAppAll}
+						toggleAppAction={toggleAppAction}
+						togglePerm={togglePerm}
+						onSavePermissions={handleSave}
+						isLoading={isLoading}
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
