5181cbba9751ac5655a0a7b21980bb47f9b6730c
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-05-26 16:15:02 +0530

    fixed filters

--- src/modules/marketing/activity-planner/api/epc.api.ts
+++ src/modules/marketing/activity-planner/api/epc.api.ts
@@ -1,55 +1,46 @@
 import qs from "qs";
 import { ServerAxios } from "../../../../services/ServerAxios";
 import type {
-	EpcCreatePayload,
-	EpcDetailResponse,
-	EpcListParams,
-	EpcListResponse,
-	EpcUpdatePayload,
+  EpcCreatePayload,
+  EpcDetailResponse,
+  EpcListParams,
+  EpcListResponse,
+  EpcUpdatePayload,
 } from "../types/epc.types";
 
 export const epcApi = {
-	getList: async (params: EpcListParams): Promise<EpcListResponse> => {
-		const response = await ServerAxios.get("/epc", {
-			params,
-			paramsSerializer: (params) => {
-				return qs.stringify(
-					{
-						...params,
-						zone: params.zone ? JSON.stringify(params.zone) : undefined,
-						status: params.status ? JSON.stringify(params.status) : undefined,
-						eventType: params.eventType
-							? JSON.stringify(params.eventType)
-							: undefined,
-					},
-					{
-						encode: false,
-					},
-				);
-			},
-		});
+  getList: async (params: EpcListParams): Promise<EpcListResponse> => {
+    const response = await ServerAxios.get("/epc", {
+      params,
+      paramsSerializer: (params) => {
+        return qs.stringify(params, {
+          arrayFormat: "repeat",
+          skipNulls: true,
+        });
+      },
+    });
 
-		return response.data;
-	},
+    return response.data;
+  },
 
-	getById: async (epcId: string): Promise<EpcDetailResponse> => {
-		const {
-			data: { data },
-		} = await ServerAxios.get(`/epc/${epcId}`);
-		return data;
-	},
+  getById: async (epcId: string): Promise<EpcDetailResponse> => {
+    const {
+      data: { data },
+    } = await ServerAxios.get(`/epc/${epcId}`);
+    return data;
+  },
 
-	create: async (payload: EpcCreatePayload) => {
-		const {
-			data: { data },
-		} = await ServerAxios.post("/epc", payload);
-		return data;
-	},
+  create: async (payload: EpcCreatePayload) => {
+    const {
+      data: { data },
+    } = await ServerAxios.post("/epc", payload);
+    return data;
+  },
 
-	update: async (epcId: string, payload: EpcUpdatePayload) => {
-		const {
-			data: { data },
-		} = await ServerAxios.put(`/epc/${epcId}`, payload);
-		return data;
-	},
+  update: async (epcId: string, payload: EpcUpdatePayload) => {
+    const {
+      data: { data },
+    } = await ServerAxios.put(`/epc/${epcId}`, payload);
+    return data;
+  },
 };
