0fdda5d44c7ce4224ab878b3a7c2f3bb201795c0
TH-Developer-Account <powerbi.dev@tatahitachi.co.in>
2026-06-25 11:31:25 +0530

    minor fix

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -5,7 +5,7 @@ export function registerServiceWorker() {
   if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
       navigator.serviceWorker
-        .register("/service-worker.js")
+        .register("./service-worker.js")
         .then((registration) => {
           console.log("SW registered:", registration.scope);
         })
